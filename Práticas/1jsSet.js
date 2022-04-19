fd899170a16ad94ffffb19eff042bd1be1756350
JuniorAndre <65295725+JuniorAndre@users.noreply.github.com>
2022-04-19 18:19:30 -0300

    change cod

--- Práticas/1jsSet.js
+++ Práticas/1jsSet.js
@@ -3,7 +3,7 @@ const myArray = [30,30,40,5,223,2049,3034,5];
 function valueUnique(array){
     const mySet = new Set(array);
 
-    return mySet;
+    return [...mySet];
 }
 
 console.log(valueUnique(myArray))
