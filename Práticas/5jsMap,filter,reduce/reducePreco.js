e406a4dcec7da92aea6a864cff52f7d2e261d563
JuniorAndre <65295725+JuniorAndre@users.noreply.github.com>
2022-05-02 20:07:31 -0300

    create file

---
+++ Práticas/5jsMap,filter,reduce/reducePreco.js
@@ -0,0 +1,9 @@
+function somaNum(arr) {
+	if (!arr || !arr.length) return;
+
+	const somaArr = arr.reduce((item,,,,,) => item % 2 === 0);
+
+	return filteredArr;
+}
+
+console.log(filtraPares([1, 2, 3, 4]));
