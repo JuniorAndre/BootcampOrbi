9f72731974fd97cf4bd5c7d04ab7b8aa65e28e16
JuniorAndre <65295725+JuniorAndre@users.noreply.github.com>
2022-05-04 01:01:51 -0300

    updat

--- Práticas/6jsDOM/assets/scripts.js
+++ Práticas/6jsDOM/assets/scripts.js
@@ -1,12 +1,22 @@
+const titulo = document.getElementById("page-title");
+const rodape = document.getElementsByTagName("footer")[0];
+const corpo = document.getElementsByTagName("body")[0];
 const botao = document.getElementById("mode-selector");
-botao.addEventListener("click", modoDark);
+botao.addEventListener('click', modoDark);
 
-function modoDark(){
-    const titulo = document.getElementById("h1");
-    const rodape = document.getElementsByTagName("footer");
-    const corpo = document.getElementsByTagName("body");
-    titulo.classList.toggle("dark-mode");
+function modoDark() {
     rodape.classList.toggle("dark-mode");
     botao.classList.toggle("dark-mode");
     corpo.classList.toggle("dark-mode");
+    changeText();
+}
+
+function changeText() {
+    if(botao.classList.contains('dark-mode')){
+        botao.innerHTML = "Light Mode";
+        titulo.innerHTML = "Dark Mode ON";
+        return;
+    }
+    botao.innerHTML = "Dark Mode";
+    titulo.innerHTML = "Light Mode ON";
 }
