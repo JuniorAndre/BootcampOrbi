e96d0e8bbf7eb8cd97a664e71c52a3cfa4a125b6
JuniorAndre <65295725+JuniorAndre@users.noreply.github.com>
2022-05-06 18:51:05 -0300

    update

---
+++ Desafios/4. Criando jogo da memória Genius/assets/scripts.js
@@ -0,0 +1,15 @@
+let order = [];
+let clikedOrder = [];
+let score = 0
+
+//0 - verde
+//1 - vermelho
+//2 - amarelo
+//3 - azul
+
+const green = document.querySelector('.green');
+const red = document.querySelector('.red');
+const yellow = document.querySelector('.yellow');
+const blue = document.querySelector('.blue');
+
+
