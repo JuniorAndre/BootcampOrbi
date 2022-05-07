80f5252bda7fec10d0868618c5c600717a8e2ccc
JuniorAndre <65295725+JuniorAndre@users.noreply.github.com>
2022-05-07 20:35:54 -0300

    update

--- Desafios/5. Criando um space shooter/assets/scripts.js
+++ Desafios/5. Criando um space shooter/assets/scripts.js
@@ -24,3 +24,33 @@ function moveUp(){
         yourShip.style.top = `${position}px`;
     }
 }
+
+function moveDown(){
+    let topPosition = getComputedStyle(yourShip).getPropertyValue('top');
+    if(topPosition === "520px"){
+        return;
+    } else{
+        let position = parseInt(topPosition);
+        position += 50;
+        yourShip.style.top = `${position}px`;
+    }
+}
+
+function fireLaser(){
+    let laser = createLaserElement();
+    playArea.appendChild(laser);
+    moveLaser();
+}
+
+function createLaserElement(){
+    let xPosition = parseInt(window.getComputedStyle(yourShip).getPropertyValue('left'));
+    let yPosition = parseInt(window.getComputedStyle(yourShip).getPropertyValue('top'));
+    let newLaser = document.createElement('img');
+    newLaser.src = 'assets/img/';
+    newLaser.classList.add('laser');
+    newLaser.style.left = `${xPosition}px`
+    newLaser.style.top = `${yPosition - 10}px`
+    return newLaser;
+}
+
+window.addEventListener('keydown', flyShip);
