d8954508e8678f0e1733af9b5c952f5595c91bb5
JuniorAndre <65295725+JuniorAndre@users.noreply.github.com>
2022-05-07 22:42:20 -0300

    update

--- Desafios/5. Criando um space shooter/assets/scripts.js
+++ Desafios/5. Criando um space shooter/assets/scripts.js
@@ -63,6 +63,15 @@ function createLaserElement(){
 function moveLaser(laser){
     let laserInterval = setInterval(() => {
         let xPosition = parseInt(laser.style.left);
+        let aliens = document.querySelectorAll('.alien');
+
+        aliens.forEach((alien => {
+            if(checkLaserCollision(laser,alien){
+                alien.src = 'assets/img/ExplosionAlien.png';
+                
+            })
+        }))
+
 
         if(xPosition === 340){
             laser.remove();
@@ -72,6 +81,7 @@ function moveLaser(laser){
     }, 10);
 }
 
+//criar aliens aleatórios
 function createAliens(){
     let newAlien = document.createElement('img');
     let alienSprite = aliensImg[Math.floor(Math.random() * aliensImg.length)] //sorteio
@@ -84,8 +94,41 @@ function createAliens(){
     moveAlien();
 }
 
+//mover alien
 function moveAlien(){
-    
+    let moveAlienInterval = setInterval(() => {
+        let xPosition = parseInt(window.getComputedStyle(alien).getPropertyValue('left'));
+
+        if(xPosition <= 50){
+            if(Array.from(alien.classList).includes('dead-alien')){
+                alien.remove();
+            }else{
+                gameOver();
+            }
+        }else{
+            alien.style.left = `${xPosition - 4}px`;
+        }
+    }, 30);
 }
+
+//colisão do laser
+function checkLaserCollision(laser,alien){
+    let laserTop = parseInt(laser.style.top);
+    let laserLeft = parseInt(laser.style.left);
+    let laserBottom = laserTop - 20;
+    let alienTop = parseInt(alien.style.top);
+    let alienLeft = parseInt(alien.style.left);
+    let alienBottom = alienTop - 30;
+    if(laserLeft !== 340 && laserLeft + 40 >= alienLeft){
+        if(laserTop <= alienTop && laserTop >= alienBottom){
+            return true;
+        }else{
+            return false;
+        }
+    }else{
+        return false;
+    }
+} 
+
 
 window.addEventListener('keydown', flyShip);
